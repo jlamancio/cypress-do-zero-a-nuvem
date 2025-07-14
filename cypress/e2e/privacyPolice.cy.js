6cfa1818cb56d50205fe0b100a2ba143420e86df
Amancio <amancio.jluis@outlook.com>
2025-07-14 14:52:50 -0300

    JLA - Npm Script para simular a execução em dispositivos mobile

---
+++ cypress/e2e/privacyPolice.cy.js
@@ -0,0 +1,10 @@
+describe('Validar página de política de privacidade', () => {
+    it('testa a página da política de privacidade de forma independente', () => {
+        cy.visit('./src/privacy.html');
+        cy.contains('p', 'Talking About Testing')
+          .should('be.visible');
+
+    })
+
+})
+
