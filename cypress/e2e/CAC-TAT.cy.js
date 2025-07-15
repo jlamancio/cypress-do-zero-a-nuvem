9b5572c78354eca5b3fce96bae1595992e93f6d8
Amancio <amancio.jluis@outlook.com>
2025-07-15 08:11:08 -0300

    JLA - Requisições HTTP com cy.request(); exercício final

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -218,4 +218,59 @@
 
   })
 
+  //* funcionalidades avançadas 
+
+  it('exibe e ocukta as mensagens de sucesso e erro utilizando : .invoke', () => {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso')
+      .invoke('hide')
+      .should('not.be.visible')
+    
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios')
+      .invoke('hide')
+      .should('not.be.visible');
+    
+  })
+
+  it('preenche o campo da área de texto usando o comando invoke', () => {
+    cy.get('#open-text-area')
+      .invoke('val','Teste de utilização do comando .invoke')
+      .should('have.value', 'Teste de utilização do comando .invoke');
+ 
+  })
+
+  it('faz uma requisição htpp', () => {
+    // ~ as requisições criadas via Postman.
+    cy.request('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
+      .as('getRequest')
+      .its('status')
+      .should('be.equal', 200);
+    
+      cy.get('@getRequest')
+        .its('statusText')
+        .should('be.equal', 'OK');
+
+      cy.get('@getRequest')
+        .its('body')
+        .should('include', 'CAC TAT');
+
+  })
+
+  it('encontra o gato escondido', () => {
+    cy.get('#cat')
+      .invoke('show')
+      .should('be.visible');
+    
+    cy.get('#title')
+      .invoke('text', 'CAT TAT');   
+
+  })
+
 })
